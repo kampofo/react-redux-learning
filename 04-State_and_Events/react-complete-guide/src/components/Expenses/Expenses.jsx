d9df44202ff9d9e533b9f0ed09645850115ffe07
Kwabena Ampofo <kampof1@students.towson.edu>
2022-01-07 12:18:39 +0000

    Complteted state and events section

---
+++ 04-State_and_Events/react-complete-guide/src/components/Expenses/Expenses.jsx
@@ -0,0 +1,47 @@
+import React, { useState } from "react";
+
+import "./Expenses.css";
+import ExpenseItem from "./ExpenseItem";
+import ExpensesFilter from "./ExpensesFilter";
+import Card from "../UI/Card";
+
+const Expenses = (props) => {
+	const [filteredYear, setFilteredYear] = useState("2022");
+
+	const filterChangeHandler = (selectedYear) => {
+		setFilteredYear(selectedYear);
+	};
+
+	return (
+		<div>
+			<Card className="expenses">
+				<ExpensesFilter
+					selected={filteredYear}
+					onChangeFilter={filterChangeHandler}
+				/>
+				<ExpenseItem
+					title={props.items[0].title}
+					date={props.items[0].date}
+					amount={props.items[0].amount}
+				/>
+				<ExpenseItem
+					title={props.items[1].title}
+					date={props.items[1].date}
+					amount={props.items[1].amount}
+				/>
+				<ExpenseItem
+					title={props.items[2].title}
+					date={props.items[2].date}
+					amount={props.items[2].amount}
+				/>
+				<ExpenseItem
+					title={props.items[3].title}
+					date={props.items[3].date}
+					amount={props.items[3].amount}
+				/>
+			</Card>
+		</div>
+	);
+};
+
+export default Expenses;
